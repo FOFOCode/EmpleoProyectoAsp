c33cc0dc1c8103ca0333cf329ecffdf57af6651c
Rodolfo Rivas Rodriguez <128103152+FOFOCode@users.noreply.github.com>
2025-03-13 14:29:02 -0600

    Modelos postulantes hecho falta hacer vista y metdo para mostrarlo y conctar a las ofertas de empleo

---
+++ PadillaEmpleosParteFofo/Models/Postulante.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PadillaEmpleosParteFofo.Models
+{
+    public class Postulante
+    {
+        [Key]
+        public int id_postulante { get; set; }
+
+        [ForeignKey("Usuario")]
+        public int id_usuario { get; set; }
+
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+        public Usuario Usuario { get; set; }
+    }
+}
---
+++ PadillaEmpleosParteFofo/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PadillaEmpleosParteFofo.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int id_usuario { get; set; }
+        public string email { get; set; }
+        public string contrasenia { get; set; }
+        public string tipo_usuario { get; set; }
+    }
+}
